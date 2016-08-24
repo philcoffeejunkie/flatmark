35dcd6b1babbb23bdbfd3bcca078148bb557c153
philcoffeejunkie <deathinfektor@gmx.de>
2016-08-24 18:57:18 +0200

    loading via js and delete functionality

--- flatmark.php
+++ flatmark.php
@@ -8,25 +8,39 @@ switch ($action) {
     file_put_contents("marks.txt", $bookmark_to_save, FILE_APPEND);
     break;
 
+  case 'del':
+    $newmarks = "";
+    $id = $_GET['id'];
+    $i = 0;
+    $handle = fopen("marks.txt", "r");
+    if ($handle) {
+      while (($mark = fgets($handle)) !== false) {
+        if ($i != $id) {
+          $newmarks .= $mark;
+        }
+        $i++;
+      }
+      fclose($handle);
+    } else {
+      // error opening the file.
+    }
+    $handle2 = fopen("marks.txt", "w");
+    fwrite($handle2, $newmarks);
+    fclose($handle2);
+    break;
+
   case 'init':
   default:
     $handle = fopen("marks.txt", "r");
     if ($handle) {
       while (($mark = fgets($handle)) !== false) {
-        $markparts = explode(" ", $mark);
-        echo "<a href=\"" . $markparts[0] . "\">" . $markparts[0] . "</a> ";
-        for ($i = 1; $i < count($markparts); $i++) {
-          echo $markparts[$i] . " ";
-        }
-        echo "<br>\n";
+        echo $mark;
       }
-
       fclose($handle);
     } else {
       // error opening the file.
     }
     break;
 }
-
 
 ?>
